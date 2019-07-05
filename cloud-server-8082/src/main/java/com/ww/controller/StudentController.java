44fd6d6fe0c23f92b8693cf3b3d9459dde1a0af4
Sun <littlesheepw@gmail.com>
2019-07-05 12:14:50 +0800

    cloud-server工程中加入Swagger2 自动生成接口文档

--- cloud-server-8082/src/main/java/com/ww/controller/StudentController.java
+++ cloud-server-8082/src/main/java/com/ww/controller/StudentController.java
@@ -3,6 +3,8 @@ package com.ww.controller;
 import com.ww.pvo.StudentPvo;
 import com.ww.service.StudentService;
 import com.ww.vo.StudentVo;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
@@ -38,6 +40,7 @@ public class StudentController {
      * @author: Sun
      * @date: 2019-06-18 18:32
      */
+    @ApiOperation(value = "获取所有学生列表" ,notes = "123")
     @RequestMapping(method = RequestMethod.GET)
     public List<StudentVo> findAll() {
         log.info("[findAll] [无入参]");
@@ -52,6 +55,8 @@ public class StudentController {
      * @author: Sun
      * @date: 2019-06-18 18:32
      */
+    @ApiOperation(value = "获取用户详细信息", notes = "根据用户id查询用户详细信息")
+    @ApiImplicitParam(name = "id", value = "用户id", required = true, dataType = "Integer")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public StudentVo findById(@PathVariable(value = "id") Integer studentId) {
         log.info("[findById] [入参] [studentId:{}]", studentId);
@@ -66,6 +71,8 @@ public class StudentController {
      * @author: Sun
      * @date: 2019-06-18 18:32
      */
+    @ApiOperation(value = "创建学生", notes = "根据Student对象创建学生")
+    @ApiImplicitParam(name = "studentPvo", value = "student对象", required = true, dataType = "StudentPvo")
     @RequestMapping(method = RequestMethod.POST)
     public StudentVo saveStudent(@RequestBody StudentPvo studentPvo) {
         log.info("[saveStudent] [入参] [studentPvo:{}]", studentPvo);
@@ -80,11 +87,12 @@ public class StudentController {
      * @author: Sun
      * @date: 2019-06-18 18:33
      */
+    @ApiOperation(value = "更改学生", notes = "根据Student对象更改学生")
+    @ApiImplicitParam(name = "studentPvo", value = "student对象", required = true, dataType = "StudentPvo")
     @RequestMapping(method = RequestMethod.PUT)
     public void updateStudent(@RequestBody StudentPvo studentPvo) {
         log.info("[updateStudent] [入参] [studentPvo:{}]", studentPvo);
         studentService.updateStudent(studentPvo);
-
     }
 
     /**
@@ -95,6 +103,8 @@ public class StudentController {
      * @author: Sun
      * @date: 2019-06-18 18:33
      */
+    @ApiOperation(value = "删除学生", notes = "根据学生id删除对应学生")
+    @ApiImplicitParam(name = "id", value = "学生id", required = true, dataType = "Integer")
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public void deleteStudent(@PathVariable(value = "id") Integer studentId) {
         log.info("[deleteStudent] [入参] [studentId:{}]", studentId);
@@ -102,14 +112,15 @@ public class StudentController {
     }
 
     /**
-     * 
-     * @param: 
-     * @throws: Eureka服务发现，提供此接口供使用放来了解我们的微服务信息
-     * @return: java.lang.Object 
-     * @author: Sun     
+     *
+     * @param:
+     * @throws: Eureka服务发现，提供此接口供使用者来了解我们的微服务信息
+     * @return: java.lang.Object
+     * @author: Sun
      * @date: 2019-06-18 18:33
-     */ 
-    @RequestMapping(value = "/discovery")
+     */
+    @ApiOperation(value = "服务发现", notes = "供使用者来了解我们的微服务信息")
+    @RequestMapping(value = "/discovery", method = RequestMethod.GET)
     public DiscoveryClient discovery() {
         List<String> services = discoveryClient.getServices();
         System.out.println("services list:" + services);
